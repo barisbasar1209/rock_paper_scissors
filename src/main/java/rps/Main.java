8020835c29b255a9aeeb6d6f38f27d4ff3fb60ab
barisbasar1209 <basar.baris1209@gmail.com>
2023-09-19 14:45:22 +0200

    test

--- src/main/java/rps/Main.java
+++ src/main/java/rps/Main.java
@@ -4,22 +4,22 @@ public class Main{
 	
 	public static void main(String[] args) throws Exception{
 			int direction = -1; 
-			int x = 0; 
+			int x = 1; 
 			int n = 1000; 
+			String s = ""; 
+			String o = "o"; 
 			while (n != 0){
 				Thread.sleep(500);
 				if(x==9 || x==0) {
 						direction = (-1) * direction; 
 						System.out.println("direction changed to "+direction);
 				}
-				System.out.println(x); 
-				Thread.sleep(1000);
 				System.out.print("\033[H\033[2J");  
-				System.out.flush();
-				System.out.printf("%"+x+"s\n","o");
+				System.out.flush(); 
+				s = String.format("%"+ x + "s", o);
+				System.out.println(s); 
 				x += direction*1; 
-				System.out.println(x);
 				n--; 
 			}
-	}	
+	}
 }
