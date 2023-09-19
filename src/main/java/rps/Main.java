6dd163eee0e4a8f9ac7f1b75b89590f243919e0d
barisbasar1209 <basar.baris1209@gmail.com>
2023-09-19 13:03:35 +0200

    initial

---
+++ src/main/java/rps/Main.java
@@ -0,0 +1,28 @@
+package rps; 
+
+public class Main{
+	
+	public static void main(String[] args) throws Exception{
+			int direction = -1; 
+			int x = 0; 
+			int n = 1000; 
+			while (n != 0){
+				Thread.sleep(500);
+				if(x==9 || x==0) {
+						direction = (-1) * direction; 
+						System.out.println("direction changed to "+direction);
+				}
+				System.out.println(x); 
+				Thread.sleep(1000);
+				System.out.print("\033[H\033[2J");  
+				if (direction==-1) {
+						System.out.printf("%"+x+"s\n","o");
+				}
+				else {
+						System.out.printf("%"+x+"s\n","o");
+				}
+				x += direction*1; 
+				n--; 
+			}
+	}	
+}
