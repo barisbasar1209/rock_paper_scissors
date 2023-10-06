dc522963b67f102a2b198a9c513d286e518149ff
barisbasar1209 <basar.baris1209@gmail.com>
2023-10-06 21:23:22 +0200

    board rendering without duell resolution

--- src/main/java/rps/Board.java
+++ src/main/java/rps/Board.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Board {
 	
-	/*final static String reset = "\u001B[0m"; 	
+	final static String reset = "\u001B[0m"; 	
 	final static String red = "\u001B[31m"; 
 	final static String green = "\u001B[32m"; 
-	final static String yellow = "\u001B[33m"; */
+	final static String yellow = "\u001B[33m";
 	private final static char FILLER = '.'; 
 	private final static int HEIGHT = 20; 
 	private final static int WIDTH = 50; 
@@ -16,7 +16,6 @@ public class Board {
 	static public ArrayList<Rock> rockList = new ArrayList<>(); 	
 	static public ArrayList<Paper> paperList = new ArrayList<>(); 	
 	static public ArrayList<Scissors> scissorsList = new ArrayList<>(); 	
-	
 	private static void createPlayers() throws Exception{
 			int rand; 
 			int x; 
@@ -44,50 +43,30 @@ public class Board {
 					}
 			}
 	} 
-	// TODO: remove player that has been defeated from board 
-	private static char resolveDuell(char next, char current){
-		if(next == current || current == FILLER) return next; 
-		if(next == 'r' && current == 'p') return 'p'; 
-		if(next == 'r' && current == 's') return 'r'; 
-		if(next == 'p' && current == 'r') return 'p'; 
-		if(next == 'p' && current == 's') return 's'; 
-		if(next == 's' && current == 'p') return 's'; 
-		if(next == 's' && current == 'r') return 'r'; 
-		System.out.println("Error: Duell could not be resolved properly! next is " + next + " current is " + current + "."); 
-		System.exit(1); 
-		return 'x'; 
-	}
-	private static void printBoard(){
-			String line = ""; 
-			String paddedLine = Util.padLeft(line, WIDTH, FILLER); 
-			line = paddedLine; 
-			char next; 
-			for(int i=0; i<HEIGHT; i++){
-				for(Rock rock : rockList){
-					if (rock.getY() == i){
-						next = resolveDuell(rock.getIcon(), line.charAt(rock.getX())); 
-						//line = Util.insertIcon(line, rock.getX(), green + next + reset); 		
-						line = Util.replaceIdx(line, rock.getX(), next); 		
-					}	
+	private static void renderBoard(){
+		Player[][] board = new Player[HEIGHT][WIDTH]; 	
+		int x; 
+		// rendering the board row by row while also resolving occuring duells
+		for (int y = 0; y<HEIGHT; y++){
+			for (Rock rock : rockList){
+				if (rock.getY() == y){
+					x = rock.getX(); 
+					board[y][x] = rock; 
 				}	
-				for(Paper paper : paperList){
-					if (paper.getY() == i){
-						next = resolveDuell(paper.getIcon(), line.charAt(paper.getX())); 
-						//line = Util.insertIcon(line, paper.getX(), yellow + next + reset); 	
-						line = Util.replaceIdx(line, paper.getX(), next); 	
-					}	
-				}
-				for(Scissors scissors : scissorsList){
-					if (scissors.getY() == i){
-						next = resolveDuell(scissors.getIcon(), line.charAt(scissors.getX())); 
-						//line = Util.insertIcon(line, scissors.getX(), red + next + reset); 	
-						line = Util.replaceIdx(line, scissors.getX(), next); 	
-					}	
-				}
-				System.out.println(line); 
-				line = paddedLine; 
 			}
-			
+			for (Paper paper : paperList){
+				if (paper.getY() == y){
+					x = paper.getX(); 
+					board[y][x] = paper; 	
+				}	
+			}
+			for (Scissors scissors : scissorsList){
+				if (scissors.getY() == y){
+					x = scissors.getX(); 
+					board[y][x] = scissors; 	
+				}	
+			}
+		}
 	}
 	private static void movePlayers(){
 		// width and height have do be decremented for the move method to work 
@@ -104,10 +83,11 @@ public class Board {
 	public void runGame() throws Exception{
 			createPlayers(); 
 			Util.refresh(); 
+			
 			while(!((rockList.size()+paperList.size() == 0) || (rockList.size()+scissorsList.size() == 0) || (paperList.size()+scissorsList.size() == 0))){
 				movePlayers(); 
 				printBoard();
-				Thread.sleep(1000); 
+				Thread.sleep(150); 
 				Util.refresh(); 
 			}
 	}
