c4233a4d02121b4a04ceaa25e217ca6ba85b75bf
barisbasar1209 <basar.baris1209@gmail.com>
2023-10-09 18:59:08 +0200

    debugged frame class. seems to work now

--- src/main/java/rps/Main.java
+++ src/main/java/rps/Main.java
@@ -10,8 +10,18 @@ public class Main {
 				for (int i = 0; i < 10; i++){
 						System.out.println(b.players.get(i).getId());
 				}*/
-				Board b = new Board(); 
+				/*Board b = new Board(); 
 				System.out.println("starting a new game"); 
-				b.runGame(); 
+				b.runGame(); */
+
+				Frame frame = new Frame(); 
+				String string = "testing out the new frame class !"; 
+				//frame.setSection(0,0,string); 
+				//System.out.println(frame.toString()); 
+				//frame.clear(); 
+				//System.out.println(frame.toString()); 
+				//String string = "testing out the new frame class !"; 
+				frame.setSection(180,10,string); 
+				System.out.println(frame.toString()); 
 		}
 }
